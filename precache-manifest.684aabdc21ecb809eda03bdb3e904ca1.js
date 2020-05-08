4af9130a8fb3046b48736dab1ec67247c5d31afc
Evgeny Orekhov <thebettermay@gmail.com>
2020-05-08 09:45:35 +0300

    Updates

---
+++ precache-manifest.684aabdc21ecb809eda03bdb3e904ca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92204cef49e8dbada27fa452dd8cc7f",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "3949fd994b11c3f14d1d",
+    "url": "/tictactoe/static/css/main.4932355b.chunk.css"
+  },
+  {
+    "revision": "a740ed8f3265a1da9ecc",
+    "url": "/tictactoe/static/js/2.af71590a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.af71590a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3949fd994b11c3f14d1d",
+    "url": "/tictactoe/static/js/main.2c7a9282.chunk.js"
+  },
+  {
+    "revision": "0296dfef4fc7a36c4d4a",
+    "url": "/tictactoe/static/js/runtime-main.f028c8b1.js"
+  }
+]);
